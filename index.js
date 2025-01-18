f4d42f4da7a2a6ab660ac416f20896f0d1ea014c
balijawa hussein <balijawahussein@gmail.com>
2025-01-18 18:36:19 +0300

    CREATE : CARDS

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
+
+  var swiperScrollbar = new Swiper(".mySwiperScrollbar", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    }
+  });
   const swiped = document.querySelectorAll(".swiper-slide");
   const start = document.getElementsByClassName("start");
   swiped.forEach(card => {
